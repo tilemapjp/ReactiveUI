8f75ae1ef9be28c242e9805ba302d2e79af5de40
OHTSUKA Ko-hei <kochizufan@gmail.com>
2014-01-01 02:58:43 +0900

    Component Store使えるところはComponent Storeka

--- ReactiveUI.Platforms/Android/ActivityRoutedViewHost.cs
+++ ReactiveUI.Platforms/Android/ActivityRoutedViewHost.cs
@@ -11,7 +11,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using ActionbarSherlock.App;
+//using ActionbarSherlock.App;
 
 namespace ReactiveUI.Android
 {
